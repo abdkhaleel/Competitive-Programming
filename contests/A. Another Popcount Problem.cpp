7543b04434bbae56df3a086fdab32ea12d34c224
abdkhaleel <abdkhaleel16@gmail.com>
2026-08-17 00:21:42 +0530

    contest

---
+++ contests/A. Another Popcount Problem.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <bit>
+using namespace std;
+
+void solve(){
+    int n, k;
+    cin>>n>>k;
+    int msb = bit_floor(n);
+    cout<<n*k;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
